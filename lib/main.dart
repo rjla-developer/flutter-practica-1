c5267609658f2cb3fd3b83698d709f3fb99887d9
RJDalefon <rjluengas@dalefon.mx>
2023-11-30 10:00:58 -0600

    Se construyo el proyecto de Flutter

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('data'),
+        ),
+      ),
+    ),
+  );
+}
