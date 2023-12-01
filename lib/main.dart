9cc9bd7a5ab100c26ff41b01b54faa124dd4539a
RJDalefon <rjluengas@dalefon.mx>
2023-12-01 09:52:59 -0600

    Se agregaron elementos al archivo main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('data'),
+          title: const Center(
+            child: Text('I am rich'),
+          ),
+          backgroundColor: Colors.blueGrey[900],
         ),
+        body: const Center(
+          child: Image(
+            image: NetworkImage(
+                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_640.jpg'),
+          ),
+        ),
+        backgroundColor: Colors.blueGrey,
       ),
     ),
   );
